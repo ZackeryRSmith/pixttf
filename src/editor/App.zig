1a9f14996560c47d607182cb71873a7620b639de
Dumb Bird <zackery.smith82307@gmail.com>
2026-03-29 17:35:49 -0400

    impl: GreeterDialog

--- src/editor/App.zig
+++ src/editor/App.zig
@@ -11,6 +11,7 @@ allocator: std.mem.Allocator = undefined,
 window: *dvui.Window = undefined,
 
 var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
+var should_show_greeter: bool = true;
 
 // runs before the first frame, after backend and dvui.Window.init()
 pub fn init(window: *dvui.Window) !void {
@@ -28,6 +29,18 @@ pub fn init(window: *dvui.Window) !void {
     pixttf.editor.* = try Editor.init(allocator);
 
     pixttf.theme = try Theme.fromJson(allocator, "assets/themes/classic_dark.json");
+
+    // TODO: Theme should probably do this
+    var default_font = dvui.Font.find(.{ .family = "pixelcode" });
+    default_font.size = 17;
+    var bold_font = dvui.Font.find(.{ .family = "pixelcode-bold" });
+    bold_font.size = 28;
+
+    var dvui_theme = dvui.themeGet();
+    dvui_theme.font_body = default_font;
+    dvui_theme.font_heading = bold_font;
+    dvui_theme.font_title = bold_font;
+    dvui.themeSet(dvui_theme);
 }
 
 // run as app is shutting down before dvui.Window.deinit()
@@ -37,5 +50,7 @@ pub fn deinit() void {
 
 // run each frame to do normal UI
 pub fn frame() !dvui.App.Result {
+    if (should_show_greeter)
+        should_show_greeter = pixttf.GreeterDialog.show();
     return try pixttf.editor.tick();
 }
--- src/editor/dialogs/GreeterDialog.zig
+++ src/editor/dialogs/GreeterDialog.zig
@@ -1,12 +1,100 @@
 const std = @import("std");
-const pixttf = @import("pixttf");
+const pixttf = @import("root");
 const dvui = @import("dvui");
 
 const GreeterDialog = @This();
 
-window: dvui.FloatingWindowWidget = undefined,
-
 pub fn show() bool {
+    // const uniqueId = dvui.parentGet().extendId(@src(), 0);
+
+    var dialog_win = dvui.floatingWindow(@src(), .{
+        .modal = true,
+        .stay_above_parent_window = true,
+        .resize = .none,
+    }, .{
+        .max_size_content = .width(600),
+        .background = false,
+        .border = .all(0),
+        .gravity_x = 0.5,
+        .gravity_y = 0.5,
+    });
+    defer dialog_win.deinit();
+    dialog_win.drag_area = undefined;
+
+    // const extra_stuff: *bool = dvui.dataGetPtrDefault(null, dialog_win.data().id, "extra_stuff", bool, false);
+    // const render_offscreen: *bool = dvui.dataGetPtrDefault(null, dialog_win.data().id, "render_offscreen", bool, true);
+    // const alpha: *f32 = dvui.dataGetPtrDefault(null, dialog_win.data().id, "alpha", f32, 1.0);
+
+    // background for dialog_win (since it has background false)
+    var back = dvui.box(@src(), .{}, .{
+        .expand = .both,
+        .style = .window,
+        .background = true,
+        .border = .all(2),
+        .min_size_content = .width(600),
+        .color_fill = pixttf.theme.color.bg_panel_dark,
+        .color_border = pixttf.theme.color.border_focus,
+    });
+    defer back.deinit();
+
+    {
+        const header = dvui.box(@src(), .{}, .{
+            .expand = .horizontal,
+            .background = true,
+            .color_fill = pixttf.theme.color.bg_app,
+        });
+        defer header.deinit();
+
+        const banner = dvui.image(@src(), .{
+            .source = dvui.ImageSource{
+                .imageFile = .{
+                    .bytes = pixttf.assets.files.@"banner.png",
+                },
+            },
+            .shrink = .both,
+        }, .{
+            .background = true,
+            .color_fill = pixttf.theme.color.bg_app,
+            .max_size_content = .{ .w = 760 * 0.5, .h = 290 * 0.5 },
+            .gravity_x = 0.5,
+        });
+        _ = banner;
+    }
+
+    var hbox = dvui.box(@src(), .{ .dir = .horizontal, .equal_space = true }, .{ .expand = .horizontal, .margin = .all(20) });
+    defer hbox.deinit();
+
+    var title_font: dvui.Font = .theme(.title);
+    title_font.size = 32;
+
+    var button_font: dvui.Font = .theme(.body);
+    button_font.size = 18;
+
+    {
+        var vbox = dvui.box(@src(), .{ .dir = .vertical }, .{ .expand = .vertical });
+        defer vbox.deinit();
+
+        dvui.label(@src(), "Start", .{}, .{ .font = title_font, .color_text = pixttf.theme.color.accent });
+        _ = dvui.button(@src(), "New File (Ctrl+N)", .{}, .{ .background = false, .font = button_font });
+        _ = dvui.button(@src(), "Open File (Ctrl+O)", .{}, .{ .background = false, .font = button_font });
+        _ = dvui.button(@src(), "Import TTF (Ctrl+T)", .{}, .{ .background = false, .font = button_font });
+    }
+
+    {
+        var vbox = dvui.box(@src(), .{ .dir = .vertical }, .{ .expand = .vertical });
+        defer vbox.deinit();
+
+        dvui.label(@src(), "Read", .{}, .{ .font = title_font, .color_text = pixttf.theme.color.accent });
+        _ = dvui.button(@src(), "Manual", .{}, .{ .background = false, .font = button_font });
+        _ = dvui.button(@src(), "Release Notes", .{}, .{ .background = false, .font = button_font });
+        _ = dvui.button(@src(), "About", .{}, .{ .background = false, .font = button_font });
+    }
+
+    // TODO: temp fix so I can commit my changes
+    if (dvui.button(@src(), "close me plz", .{}, .{ .background = false, .font = button_font })) {
+        return false;
+    }
+
     return true;
 }
 
